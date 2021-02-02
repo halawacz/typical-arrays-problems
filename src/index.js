03d9b6892fb831265b5b4336a1ad4d8f6490983c
Dmitry Halavach <halawacz@gmail.com>
2021-02-02 17:41:24 +0300

    solution2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array = [0]) {
+    let minimum = Math.min(...array);
+    let result = isFinite(minimum) ? minimum : 0;
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = [0]) {
+    let maximum = Math.max(...array);
+    let result = isFinite(maximum) ? maximum : 0;
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = [0]) {
+    let average = array.reduce((a, b) => a + b, 0) / array.length;
+    let result = isNaN(average) ? 0 : average;
+    return result;
+};
